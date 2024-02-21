55e69e34caf57031eb576f38a02992670b442522
Jesus Antonio Ramirez <tonchirmz@gmail.com>
2024-02-21 02:10:16 -0600

    Ejemplo chi

--- example.test.js
+++ example.test.js
@@ -1,6 +1,14 @@
 // example.test.js
-const { sum } = require('./example');
+const sum = require('./example');
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
+});
+
+test('adds 5 + 7 to equal 12', () => {
+  expect(sum(5, 7)).toBe(12);
+});
+
+test('adds -3 + 7 to equal 4', () => {
+  expect(sum(-3, 7)).toBe(4);
 });
